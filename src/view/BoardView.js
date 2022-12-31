0d5ccfb552e70988f33e75cd401915212373d0e9
kyw0716 <kyw0716@naver.com>
2022-12-31 17:52:44 +0900

    refactor: 보드 출력 함수 분리

--- src/view/BoardView.js
+++ src/view/BoardView.js
@@ -13,26 +13,45 @@ export default class BoardView {
     newBoard.style =
       "width: 660px; height: 480px; border: 1px solid black; display: flex; flex-direction: column; justify-content: center; align-items: center;";
 
+    this.fillTile(width, height, newBoard);
+
+    this.#container.appendChild(newBoard);
+  }
+
+  fillTile(width, height, newBoard) {
     for (let i = 0; i < height; i++) {
-      const subContainer = document.createElement("div");
-      subContainer.id = `${i}`;
-      subContainer.style = `display: flex;`;
+      const subContainer = this.createTileRowContainer(i);
 
       for (let j = 0; j < width; j++) {
-        if (i % 2 === 0) {
-          if (j % 2 === 0)
-            new Tile(i, j, "rgb(235, 235, 235)").printTile(subContainer);
-          else new Tile(i, j, "rgb(246, 246, 246)").printTile(subContainer);
-        } else {
-          if (j % 2 === 0)
-            new Tile(i, j, "rgb(246, 246, 246)").printTile(subContainer);
-          else new Tile(i, j, "rgb(235, 235, 235)").printTile(subContainer);
-        }
+        const newTile = this.createTile(i, j);
+
+        newTile.printTile(subContainer);
       }
 
       newBoard.appendChild(subContainer);
     }
+  }
 
-    this.#container.appendChild(newBoard);
+  createTileRowContainer(i) {
+    const subContainer = document.createElement("div");
+
+    subContainer.id = `${i}`;
+    subContainer.style = `display: flex;`;
+
+    return subContainer;
+  }
+
+  createTile(i, j) {
+    let color;
+
+    if (i % 2 === 0) {
+      if (j % 2 === 0) color = "rgb(235, 235, 235)";
+      else color = "rgb(246, 246, 246)";
+    } else {
+      if (j % 2 === 0) color = "rgb(246, 246, 246)";
+      else color = "rgb(235, 235, 235)";
+    }
+
+    return new Tile(i, j, color);
   }
 }
