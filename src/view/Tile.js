d5878bb678fd2adc01331ac380a271f66e49db35
kyw0716 <kyw0716@naver.com>
2023-01-01 18:00:17 +0900

    style: 타일 색상 그라데이션 추가

--- src/view/Tile.js
+++ src/view/Tile.js
@@ -2,20 +2,24 @@ class Tile {
   #row;
   #column;
   #color;
+  #gradientColor;
 
   constructor(row, column, color) {
     this.#row = row;
     this.#column = column;
     this.#color = color;
+    this.#gradientColor = this.#getGradationColor();
   }
 
   printTile(container) {
     const Tile = document.createElement("div");
 
     if (!["rgb(235, 235, 235)", "rgb(246, 246, 246)"].includes(this.#color)) {
-      Tile.style = `width: 25px; height: 25px; background-color: ${
-        this.#color
-      }; border-radius: 3px; box-shadow: inset 0 0 5px white; border: 1px solid ${
+      Tile.style = `width: 25px; height: 25px; background: linear-gradient(${
+        this.#gradientColor
+      }, ${this.#color}, ${
+        this.#gradientColor
+      }); border-radius: 3px; border: 1px solid ${
         this.#color
       }; box-sizing: border-box;`;
     } else {
@@ -27,6 +31,10 @@ class Tile {
     Tile.id = `${this.#row}_${this.#column}`;
 
     container.appendChild(Tile);
+  }
+
+  #getGradationColor() {
+    return this.#color.replace(/rgb/, "rgba").slice(0, -1) + ", 0.6)";
   }
 }
 
